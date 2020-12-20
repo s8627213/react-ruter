e1fe31b4383f259bd5c63ad168d631722c2783c7
孙晨亮 <chenliang.sun@hand-china.com>
2020-12-20 14:55:19 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,64 +4,63 @@ import {
   Switch,
   Route,
   Link
-} from "react-router-dom";
+} from "react-dom";
 
 
-export default function BasicExample() {
-  return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">主页</Link>
-          </li>
-          <li>
-            <Link to="/about">关于</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">其他</Link>
-          </li>
-        </ul>
+export default function BasicExample() {return (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">主页</Link>
+        </li>
+        <li>
+          <Link to="/about">关于</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">其他</Link>
+        </li>
+      </ul>
 
-        <hr />
+      <hr />
 
-      
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+    
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+);
 }
 
 function Home() {
-  return (
-    <div>
-      <h2>主页</h2>
-    </div>
-  );
+return (
+  <div>
+    <h2>主页</h2>
+  </div>
+);
 }
 
 function About() {
-  return (
-    <div>
-      <h2>关于</h2>
-    </div>
-  );
+return (
+  <div>
+    <h2>关于</h2>
+  </div>
+);
 }
 
 function Dashboard() {
-  return (
-    <div>
-      <h2>其他</h2>
-    </div>
-  );
+return (
+  <div>
+    <h2>其他</h2>
+  </div>
+);
 }
